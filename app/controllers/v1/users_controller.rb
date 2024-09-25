88981d8f16353af1617e833755f3ad098a9805ef
Annie Pulzone <ajpulzone@gmail.com>
2024-09-25 13:44:09 -0700

    WIP

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController > ApplicationController 
 
-  def index
+  def create
+      user = User.new(user_params)
+    if user.save
+      redirect_to users_path
+    else 
+      flash[:notice] = "User not created: Required information missing or incorreect"
+      redirect_to '/admin/merchants/new'
+    end
     
+  end
+  
+  def show
+  
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
